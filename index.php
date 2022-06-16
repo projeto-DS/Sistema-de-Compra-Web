96348a19f0c055d6bc086a2f6ff33819c8d3d036
Kauã F. N. Rodrigues <77804949+rodrigueskaua@users.noreply.github.com>
2022-06-16 18:41:49 -0300

    Conexão com o banco de dados

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+
+
+ 
+  <link rel="stylesheet" href="_css/pg.css">
+
+
+  <title>Cadastro de Clientes</title>
+</head>
+    <?php
+        include "_scripts/config.php";
+    ?>
+<body>
+
+</body>
+</html>
